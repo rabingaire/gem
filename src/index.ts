d193aba49d2676a9289d619fff8ae6aa4f085b4d
Rabin Gaire <rabingaire20@gmail.com>
2021-12-09 22:19:20 +0545

    integrate base with gem editor

--- src/index.ts
+++ src/index.ts
@@ -4,32 +4,84 @@ import { EditorView } from "prosemirror-view";
 import { Node } from "prosemirror-model";
 import { undo, redo, history } from "prosemirror-history";
 import { keymap } from "prosemirror-keymap";
-import { baseKeymap } from "prosemirror-commands";
+import { baseKeymap, Keymap } from "prosemirror-commands";
 import { markdownInputRules, markdownKeyBindings } from "./markdown";
 import { CursorPlugin } from "./cursor";
 import { initalContent } from "./initial";
+import { Storage } from "./storage";
 
 export const main = document.querySelector("main")!;
 
-// Initialize state, set up plugins
-const state = EditorState.create<typeof schema>({
-  doc: Node.fromJSON(schema, initalContent),
-  schema,
-  plugins: [
-    history(),
-    keymap<typeof schema>({
-      "Mod-z": undo,
-      "Mod-y": redo,
-      ...markdownKeyBindings,
-    }),
-    keymap<typeof schema>(baseKeymap),
-    markdownInputRules,
-    CursorPlugin,
-  ],
-});
+const storage = new Storage(""); // pass base api key here
+
+const appState: { editor: EditorState<typeof schema>; buffer: number } = {
+  editor: EditorState.create<typeof schema>({
+    doc: getDoc(),
+    schema,
+    plugins: [
+      history(),
+      keymap<typeof schema>({
+        "Mod-z": undo,
+        "Mod-y": redo,
+        ...ctrlZeroToNineKeymap(),
+        ...markdownKeyBindings,
+      }),
+      keymap<typeof schema>(baseKeymap),
+      markdownInputRules,
+      CursorPlugin,
+    ],
+  }),
+  buffer: 0,
+};
 
 const view = new EditorView<typeof schema>(main, {
-  state,
+  state: appState.editor,
+  dispatchTransaction(transaction) {
+    transaction.replaceSelection;
+    update({ type: "EDITOR_TRANSACTION", payload: transaction });
+  },
 });
-
 view.focus();
+
+export function update(event: { type: string; payload: any }) {
+  if (event.type == "EDITOR_TRANSACTION") {
+    appState.editor = appState.editor.apply(event.payload);
+    storage.set(appState.buffer.toString(), appState.editor.doc.toJSON());
+  }
+
+  if (event.type == "LOAD_BUFFER") {
+    appState.buffer = event.payload as number;
+    appState.editor = appState.editor.apply(
+      appState.editor.tr.replaceWith(
+        0,
+        appState.editor.doc.content.size,
+        getDoc(appState.buffer)
+      )
+    );
+    document.querySelector(
+      ".buffer"
+    )!.textContent = `Buffer ${appState.buffer}`;
+  }
+
+  view.updateState(appState.editor);
+}
+
+function getDoc(buffer = 0): Node<typeof schema> {
+  const value = storage.get(buffer.toString());
+  return Node.fromJSON(schema, value ? value : initalContent);
+}
+
+function ctrlZeroToNineKeymap(): Keymap<typeof schema> {
+  const keymap: Keymap<typeof schema> = {};
+  for (let i = 0; i < 10; i++) {
+    keymap[`Ctrl-${i}`] = ctrl(i);
+  }
+  return keymap;
+}
+
+function ctrl(index: number): () => boolean {
+  return function (): boolean {
+    update({ type: "LOAD_BUFFER", payload: index });
+    return true;
+  };
+}
---
+++ src/storage.ts
@@ -0,0 +1,56 @@
+import { Deta } from "deta";
+import { update } from "./index";
+
+export class Storage {
+  constructor(apiKey = "") {
+    if (apiKey.length > 0) {
+      const db = Deta(apiKey).Base("gem");
+
+      // hydrate local storage
+      if (this.getLocalStorageKeys().length == 0) {
+        db.fetch().then(({ items }) => {
+          items.forEach((item: { [key: string]: any }) => {
+            localStorage.setItem(item.key, item.value);
+          });
+          update({ type: "LOAD_BUFFER", payload: 0 });
+        });
+      }
+
+      setInterval(() => {
+        const values = this.getLocalStorageValues();
+        if (values.length > 0) {
+          db.putMany(values);
+        }
+      }, 3000);
+    }
+  }
+
+  private getLocalStorageKeys(): string[] {
+    return Object.keys(localStorage).filter((key: string) => {
+      return key.startsWith("gem-");
+    });
+  }
+
+  private getLocalStorageValues(): { [key: string]: any }[] {
+    return this.getLocalStorageKeys().map((key) => {
+      return {
+        key,
+        value: localStorage.getItem(key),
+      };
+    });
+  }
+
+  public get(key: string): { [key: string]: any } | null {
+    const value = localStorage.getItem(`gem-${key}`);
+    if (value) {
+      return JSON.parse(value);
+    }
+
+    return null;
+  }
+
+  public set(key: string, newData: any): void {
+    const newValue = JSON.stringify(newData);
+    localStorage.setItem(`gem-${key}`, newValue);
+  }
+}
